19175e51e190caa7b63a9c7634762a706860edbb
ajoy267 <andrew.joy12@hotmail.com>
2022-01-04 17:55:35 -0800

    added loader to Dogs.js

--- src/views/Dogs/Dogs.js
+++ src/views/Dogs/Dogs.js
@@ -2,21 +2,37 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { fetchDogs } from '../../services/dog';
 import DogList from '../../components/DogList/DogList';
+import './Dogs.css';
 
 export default function Dogs() {
   const [dogs, setDogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    let timer;
     const getDogs = async () => {
       const resp = await fetchDogs();
       setDogs(resp);
+      timer = setTimeout(() => {
+        setLoading(false);
+      }, 1500);
     };
-    getDogs();
-  }, []);
+    if (loading) {
+      getDogs();
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [loading]);
   return (
-    <div>
-      <h1>Dogs</h1>
-      <DogList dogs={dogs} />
-    </div>
+    <>
+      {loading && <div className="loader"></div>}
+      {!loading && (
+        <div>
+          <h1>Dogs</h1>
+          <DogList dogs={dogs} />
+        </div>
+      )}
+    </>
   );
 }
