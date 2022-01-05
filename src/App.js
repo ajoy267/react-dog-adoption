4df5796e1671b8c9ba5260cc9689c0d19e41fccb
ajoy267 <andrew.joy12@hotmail.com>
2022-01-04 17:37:15 -0800

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dogs from './views/Dogs/Dogs';
 import Dog from './views/Dog/Dog';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <header>
-          <NavLink to="/dogs">Home</NavLink>
-        </header>
+        <Header />
         <Switch>
           <Route path="/dogs" exact>
             <Dogs />
