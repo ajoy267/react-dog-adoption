16e41ef493accb05f481533503f7bac698253606
ajoy267 <andrew.joy12@hotmail.com>
2022-01-04 18:01:12 -0800

    added loader to dog.js

--- src/views/Dog/Dog.js
+++ src/views/Dog/Dog.js
@@ -3,21 +3,37 @@ import { fetchDogById } from '../../services/dog';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import DogDetails from '../../components/DogDetails/DogDetails';
+import './Dog.css';
 
 export default function Dog() {
   const [dog, setDog] = useState([]);
+  const [loading, setLoading] = useState(true);
   const params = useParams();
 
   useEffect(() => {
+    let timer;
     const fetchData = async () => {
       const data = await fetchDogById(params.id);
       setDog(data);
+      timer = setTimeout(() => {
+        setLoading(false);
+      }, 750);
     };
-    fetchData();
-  }, [params.id]);
+    if (loading) {
+      fetchData();
+    }
+    return () => {
+      clearInterval(timer);
+    };
+  }, [params.id, loading]);
   return (
-    <div>
-      <DogDetails dog={dog} />
-    </div>
+    <>
+      {loading && <div className="loader"></div>}
+      {!loading && (
+        <div>
+          <DogDetails dog={dog} />
+        </div>
+      )}
+    </>
   );
 }
